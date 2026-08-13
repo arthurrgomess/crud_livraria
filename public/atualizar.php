8b3e985082756661b4a43d7370b1eea2b682c10d
arthurrgomess <arthur_r_gomes@estudante.sesisenai.org.br>
2026-08-13 09:32:45 -0300

    add - fiz algumas alterações

--- public/atualizar.php
+++ public/atualizar.php
@@ -6,8 +6,16 @@ $id = $_POST["id"];
 $titulo = $_POST["titulo"];
 $autor = $_POST["autor"];
 $ano = $_POST["ano"];
+-----------------------------------------------------------
+$sql = "UPDATE livros SET titulo = ?, autor = ?, ano = ? WHERE id = ?";
+--------------------------------------------------------
+$stmt = mysqli_prepare($conexao, $sql);
 
-$sql = "UPDATE livros SET titulo='$titulo',autor='$autor',ano='$ano' WHERE id = '$id'";
+mysqli_stmt_bind_param($stmt, "ssii", $titulo, $autor, $ano, $id);
 
-mysqli_query($conexao, $sql);
+mysqli_stmt_execute($stmt);
+
 header("Location: ../index.php");
+exit();
+
+?>
