cb97c36732123221928af3c4500ec4bb0c8e24eb
arthurrgomess <arthur_r_gomes@estudante.sesisenai.org.br>
2026-08-13 09:32:08 -0300

    add - apliquei e  criei o Prepared Statement para ter uma melhor performace e segurança com o Mysql

--- public/cadastrar.php
+++ public/cadastrar.php
@@ -5,6 +5,7 @@ include "../infra/conexao.php";
 $titulo = $_POST["titulo"];
 $autor = $_POST["autor"];
 $ano = $_POST["ano"];
+------------------------------------ //alteração : Criei o "Prepared Statement" para ter uma segurança maior entre Mysql
 
 $sql = "INSERT INTO livros (titulo, autor, ano) VALUES (?, ?, ?)";
 
@@ -13,6 +14,7 @@ $stmt = mysqli_prepare($conexao, $sql);
 mysqli_stmt_bind_param($stmt, "ssi", $titulo, $autor, $ano);
 
 mysqli_stmt_execute($stmt);
+-----------------------------------------------
 
 header("Location: ../index.php");
 exit();
